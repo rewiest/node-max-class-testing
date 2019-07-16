b163927bd08943d12a25d388b85b4d9c87602781
Ralph Wiest <lioncrazed@gmail.com>
2019-07-16 10:01:14 -0400

    added CORS

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));   // x-www-form-urlencoded  with a <form>
 app.use(bodyParser.json());                          // application/json
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
